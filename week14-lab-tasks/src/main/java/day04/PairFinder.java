f64da78a50a44e4e59dc0dda0522fbf546a062bb
anitaszoke <szanita1735@gmail.com>
2022-01-27 16:36:41 +0100

    week14 day04 PairFinder

---
+++ week14-lab-tasks/src/main/java/day04/PairFinder.java
@@ -0,0 +1,27 @@
+package day04;
+
+import java.util.Arrays;
+
+public class PairFinder {
+
+    public int findPair(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    i = j + 1;
+                    j = i;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        PairFinder pairFinder = new PairFinder();
+        System.out.println(pairFinder.findPair(new int[]{7,7,7,7,7,8,8,9,5,5,1,2,2}));
+    }
+}
